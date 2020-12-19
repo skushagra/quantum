630781d4cd6a451efd8d06591a3a902f480a8b7c
Kushagra S <kushagra.rigel@gmail.com>
2020-12-19 16:41:38 +0530

    Update out0.py

--- python_files/out0.py
+++ python_files/out0.py
@@ -3,15 +3,19 @@
 import qiskit
 from qiskit import *
 import matplotlib
+from qiskit.tools.visualization import plot_histogram
 i = input()
 circuit = QuantumCircuit(len(i), len(i))
 for j in range(0,len(i)):
-    circuit.h(i)
-for j in range(0,len(i)):
-    circuit.x(0)
-    circuit.x(len(i))
+    circuit.h(j)
+circuit.x(0)
+circuit.x(len(i)-1)
 for j in range(1,len(i)-1,2):
-    circuit.cx(i,i+1)
+    circuit.cx(j,j+1)
 for j in range(0,len(i)):
-    circuit.h(i)
-circuit.draw(output='mpl')
+    circuit.h(j)
+# circuit.draw()
+backend = Aer.get_backend('qasm_simulator')
+result = execute(circuit, backend=backend, shots=1024).result()
+counts = result.get_counts()
+plot_histogram(counts)

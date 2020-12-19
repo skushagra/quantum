3c62fb3e6a95aa6e0f41b53e98259c729de5a848
Kushagra S <kushagra.rigel@gmail.com>
2020-12-19 16:28:33 +0530

    Create out0.py

---
+++ python_files/out0.py
@@ -0,0 +1,15 @@
+import qiskit
+from qiskit import *
+import matplotlib
+i = input()
+circuit = QuantumCircuit(len(i), len(i))
+for j in range(0,len(i)):
+    circuit.h(i)
+for j in range(0,len(i)):
+    circuit.x(0)
+    circuit.x(len(i))
+for j in range(1,len(i)-1,2):
+    circuit.cx(i,i+1)
+for j in range(0,len(i)):
+    circuit.h(i)
+circuit.draw(output='mpl')
